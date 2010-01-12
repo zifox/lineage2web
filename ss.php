cb10db14fb8525cdcb89d639351311807db739ae
antons007 <antons007@24ad18e8-e4c2-11de-8dea-4d08a2fd72f5>
2010-01-12 17:37:57 +0000

    SS

--- ss.php
+++ ss.php
@@ -42,7 +42,7 @@ $strife = $row[15]+$row[18];
 
 <title>Seven Signs Status</title>
 
-<SCRIPT language=javascript>
+<script language="javascript">
 
 var nthDay = 8;
 var currTime = 'we are at work ...';
@@ -57,19 +57,19 @@ var seal4 = 0;
 var seal5 = 0;
 var seal6 = 0;
 var seal7 = 1;
-</SCRIPT>
+</script>
 
-<TABLE style="MARGIN-TOP:0px; width:500px;" cellSpacing=0 cellPadding=0 border=0 align=center>
-                          <TBODY>
-                            <TR vAlign=top>
-                              <TD style="BACKGROUND: url(/module/ss/ssqViewBg.jpg)" height=225><TABLE>
-                                  <TBODY>
-                                    <TR vAlign=top>
-                                      <TD><TABLE style="MARGIN: 18px 0px 0px 54px" cellSpacing=0 cellPadding=0 border=0 width=141>
-                                          <TBODY>
-                                            <TR align=middle height=26>
-                                              <TD style="BACKGROUND: url(/module/ss/ssqViewimg1.gif); COLOR:#fff; font-size:11px;"><!-- Subject selection + number of day Start -->
-                                                <SCRIPT language=javascript>
+<table style="MARGIN-TOP:0px; width:500px;" cellspacing="0" cellpadding="0" border="0" align="center">
+                          <tbody>
+                            <tr valign="top">
+                              <td style="background: url(img/ss/ssqViewBg.jpg)" height="225"><table>
+                                  <tbody>
+                                    <tr valign="top">
+                                      <td><table style="MARGIN: 18px 0px 0px 54px" cellspacing="0" cellpadding="0" border="0" width="141">
+                                          <tbody>
+                                            <tr align="middle" height="26">
+                                              <td style="BACKGROUND: url(img/ss/ssqViewimg1.gif); COLOR:#fff; font-size:11px;"><!-- Subject selection + number of day Start -->
+                                                <script language="javascript">
 
 						if (0 == ssStatus) {
 							document.write('Start');
@@ -84,24 +84,24 @@ var seal7 = 1;
 							document.write('ss result<b style="color:#E10000"> day ' + nthDay + '</b>');
 						}
 					//-->
-					</SCRIPT>
+					</script>
                                                 <!-- Subject selection + number of day E n d -->
-                                              </TD>
-                                            </TR>
-                                          </TBODY>
-                                        </TABLE>
-                                        <TABLE style="MARGIN: 3px 0px 0px 10px"
-                              cellSpacing=0 cellPadding=0 width=141 border=0>
-                                          <TBODY>
-                                            <TR>
-                                              <TD></TD>
-                                              <TD><IMG height=16 src="/module/ss/timeBox1.jpg" width=140 border=0></TD>
-                                              <TD></TD>
-                                            </TR>
-                                            <TR>
-                                              <TD vAlign=bottom rowSpan=2><IMG height=125 src="/module/ss/timeBox2.jpg" width=45 border=0></TD>
-                                              <TD><!-- Clock Image Start
-											--><SCRIPT language=javascript>
+                                              </td>
+                                            </tr>
+                                          </tbody>
+                                        </table>
+                                        <table style="MARGIN: 3px 0px 0px 10px"
+                              cellspacing="0" cellpadding="0" width="141" border="0">
+                                          <tbody>
+                                            <tr>
+                                              <td></td>
+                                              <td><img height="16" src="img/ss/timeBox1.jpg" width="140" border="0" /></td>
+                                              <td></td>
+                                            </tr>
+                                            <tr>
+                                              <td valign="bottom" rowspan="2"><img height="125" src="img/ss/timeBox2.jpg" width="45" border="0" /></td>
+                                              <td><!-- Clock Image Start
+											--><script language="javascript">
 													var timeImage;
 													var tempImageNum;
 
@@ -115,117 +115,117 @@ var seal7 = 1;
 														tempImageNum = nthDay + 7;   <!-- Adding one week to show img with seal effect -->
 													}
 													timeImage = 'time'+tempImageNum+'.jpg';
-													document.write('<img src="/module/ss/time/'+ timeImage +'" width="140" height="139" border="0">');
-												</SCRIPT><!--
-												Clock Image E n d --></TD>
-                                              <TD vAlign=bottom rowSpan=2><IMG height=125 src="/module/ss/timeBox3.jpg" width=66 border=0></TD>
-                                            </TR>
-                                            <TR>
-                                              <TD><IMG height=12 src="/module/ss/timeBox4.jpg" width=140 border=0></TD>
-                                            </TR>
-                                          </TBODY>
-                                        </TABLE></TD>
-                                      <TD><TABLE style="MARGIN: 27px 0px 0px 22px" cellSpacing=0 cellPadding=0 width=200 border=0>
-                                          <TBODY>
-                                            <TR align=middle bgColor=#606d6f height=17>
-                                              <TD style="COLOR:#fff; font-size:11px;"><!-- Current Time start - Not Real Time but the time of query -->
+													document.write('<img src="img/ss/time/'+ timeImage +'" width="140" height="139" border="0" />');
+												</script><!--
+												Clock Image E n d --></td>
+                                              <td valign="bottom" rowspan="2"><img height="125" src="img/ss/timeBox3.jpg" width="66" border="0" /></td>
+                                            </tr>
+                                            <tr>
+                                              <td><img height="12" src="img/ss/timeBox4.jpg" width="140" border="0" /></td>
+                                            </tr>
+                                          </tbody>
+                                        </table></td>
+                                      <td><table style="MARGIN: 27px 0px 0px 22px" cellspacing="0" cellpadding="0" width="200" border="0">
+                                          <tbody>
+                                            <tr align="middle" bgcolor="#606d6f" height="17">
+                                              <td style="COLOR:#fff; font-size:11px;"><!-- Current Time start - Not Real Time but the time of query -->
                                                 <?php
-$timezone  = 3; //(GMT -5:00) EST (U.S. & Canada)
+$timezone  = 2; //(GMT -5:00) EST (U.S. & Canada)
 echo gmdate("Y/m/j H:i:s", time() + 3600*($timezone+date("I")));
 ?>
                                                 <!-- Current Time E n d -->
-                                              </TD>
-                                            </TR>
-                                          </TBODY>
-                                        </TABLE>
-                                        <TABLE style="MARGIN: 21px 0px 0px 22px" cellSpacing=0 cellPadding=0 border=0>
-                                          <COLGROUP>
-                                          <COL width=74>
-                                          <COL width=*>
-                                          <TBODY>
-                                            <TR>
-                                              <TD style="font-size:11px; color:#000;"><IMG style="MARGIN: 0px 6px 5px 0px" height=1 src="/ssq/ssq_image/dot.gif" width=1 border=0>Dawn</TD>
-                                              <TD style="COLOR: #000; font-size:11px;"><!-- Twilight Bar Graph Start -->
-                                                <SCRIPT language=javascript>
+                                              </td>
+                                            </tr>
+                                          </tbody>
+                                        </table>
+                                        <table style="MARGIN: 21px 0px 0px 22px" cellspacing="0" cellpadding="0" border="0">
+                                          <colgroup>
+                                          <col width="74" />
+                                          <col width="*" />
+                                          <tbody>
+                                            <tr>
+                                              <td style="font-size:11px; color:#000;"><img style="MARGIN: 0px 6px 5px 0px" height="1" src="/ssq/ssq_image/dot.gif" width="1" border="0" />Dawn</td>
+                                              <td style="COLOR: #000; font-size:11px;"><!-- Twilight Bar Graph Start -->
+                                                <script language="javascript">
 
 						var twilPointWidth = maxPointWidth * twilPoint / 100;
-						document.write('<img src="/module/ss/ssqbar2.gif" width="' + twilPointWidth + '" height="9" border="0"> ' + twilPoint);
+						document.write('<img src="img/ss/ssqbar2.gif" width="' + twilPointWidth + '" height="9" border="0" /> ' + twilPoint);
 
-					</SCRIPT>
+					</script>
                                                 <!-- Twilight Bar Graph E n d -->
-                                              </TD>
-                                            </TR>
-                                            <TR>
-                                              <TD colSpan=2 height=7></TD>
-                                            </TR>
-                                            <TR>
-                                              <TD style="font-size:11px; color:#000;"><IMG style="MARGIN: 0px 6px 5px 0px" height=1 src="/ssq/ssq_image/dot.gif" width=1 border=0>Dusk</TD>
-                                              <TD style="COLOR: #000; font-size:11px;"><!-- Dawn Bar Graph Start -->
-                                                <SCRIPT language=javascript>
+                                              </td>
+                                            </tr>
+                                            <tr>
+                                              <td colspan="2" height="7"></td>
+                                            </tr>
+                                            <tr>
+                                              <td style="font-size:11px; color:#000;"><img style="MARGIN: 0px 6px 5px 0px" height="1" src="/ssq/ssq_image/dot.gif" width="1" border="0" />Dusk</td>
+                                              <td style="COLOR: #000; font-size:11px;"><!-- Dawn Bar Graph Start -->
+                                                <script language="javascript">
 
 						var dawnPointWidth = maxPointWidth * dawnPoint / 100;
-						document.write('<img src="/module/ss/ssqbar1.gif" width="' + dawnPointWidth + '" height="9" border="0"> ' + dawnPoint);
+						document.write('<img src="img/ss/ssqbar1.gif" width="' + dawnPointWidth + '" height="9" border="0" /> ' + dawnPoint);
 
-					</SCRIPT>
+					</script>
                                                 <!-- Dawn Bar Graph E n d -->
-                                              </TD>
-                                            </TR>
-                                          </TBODY>
-                                        </TABLE>
-                                        <TABLE cellSpacing=0 cellPadding=0 border=0>
-                                          <TBODY>
-                                            <TR vAlign=bottom align=middle height=95>
-                                              <TD><!-- It not 3 then seal must be shown as grey.Seal Status 0 = closed, 1 or 2 = opened  -->
+                                              </td>
+                                            </tr>
+                                          </tbody>
+                                        </table>
+                                        <table cellspacing="0" cellpadding="0" border="0">
+                                          <tbody>
+                                            <tr valign="bottom" align="middle" height="95">
+                                              <td><!-- It not 3 then seal must be shown as grey.Seal Status 0 = closed, 1 or 2 = opened  -->
                                                 <!-- Seal #1 -->
-                                                <SCRIPT language=javascript>
+                                                <script language="javascript">
 						if (3 == ssStatus) {
 							if (0 == seal1)
-								document.write('<img src="/module/ss/Seals/SOA/bongin1close.gif" width="85" height="86" border="0">');
+								document.write('<img src="img/ss/Seals/SOA/bongin1close.gif" width="85" height="86" border="0" />');
 							else
-								document.write('<img src="/module/ss/Seals/SOA/bongin1open.gif" width="85" height="86" border="0">');
+								document.write('<img src="img/ss/Seals/SOA/bongin1open.gif" width="85" height="86" border="0" />');
 						}   else {
-							document.write('<img src="/module/ss/Seals/SOA/bongin1.gif" width="85" height="86" border="0">');
+							document.write('<img src="img/ss/Seals/SOA/bongin1.gif" width="85" height="86" border="0" />');
 						}
-					</SCRIPT>
-                                              </TD>
-                                              <TD><!-- Seal #2 -->
-                                                <SCRIPT language=javascript>
+					</script>
+                                              </td>
+                                              <td><!-- Seal #2 -->
+                                                <script language="javascript">
 						if (3 == ssStatus) {
 							if (0 == seal2)
-								document.write('<img src="/module/ss/Seals/SOG/bongin2close.gif" width="85" height="86" border="0">');
+								document.write('<img src="img/ss/Seals/SOG/bongin2close.gif" width="85" height="86" border="0" />');
 							else
-								document.write('<img src="/module/ss/Seals/SOG/bongin2open.gif" width="85" height="86" border="0">');
+								document.write('<img src="img/ss/Seals/SOG/bongin2open.gif" width="85" height="86" border="0" />');
 						}   else {
-							document.write('<img src="/module/ss/Seals/SOG/bongin2.gif" width="85" height="86" border="0">');
+							document.write('<img src="img/ss/Seals/SOG/bongin2.gif" width="85" height="86" border="0" />');
 						}
 
-					</SCRIPT>
-                                              </TD>
-                                              <TD><!-- Seal #3 -->
-                                                <SCRIPT language=javascript>
+					</script>
+                                              </td>
+                                              <td><!-- Seal #3 -->
+                                                <script language="javascript">
 						if (3 == ssStatus) {
 							if (0 == seal3)
-								document.write('<img src="/module/ss/Seals/SOS/bongin3close.gif" width="85" height="86" border="0">');
+								document.write('<img src="img/ss/Seals/SOS/bongin3close.gif" width="85" height="86" border="0" />');
 							else
-								document.write('<img src="/module/ss/Seals/SOS/bongin3open.gif" width="85" height="86" border="0">');
+								document.write('<img src="img/ss/Seals/SOS/bongin3open.gif" width="85" height="86" border="0" />');
 						}   else {
-							document.write('<img src="/module/ss/Seals/SOS/bongin3.gif" width="85" height="86" border="0">');
+							document.write('<img src="img/ss/Seals/SOS/bongin3.gif" width="85" height="86" border="0" />');
 						}
 
-					</SCRIPT>
-                                              </TD>
-                                            </TR>
-                                            <TR>
-                                              <TD colSpan=3><div align="center" style="margin-left:10px;"><IMG height=16 src="/module/ss/bonginName.gif" width=258 border=0> </div></TD>
-                                            </TR>
-                                          </TBODY>
-                                        </TABLE></TD>
-                                    </TR>
-                                  </TBODY>
-                                </TABLE></TD>
-                            </TR>
-                          </TBODY>
-                        </TABLE>
+					</script>
+                                              </td>
+                                            </tr>
+                                            <tr>
+                                              <td colspan="3"><div align="center" style="margin-left:10px;"><img height="16" src="img/ss/bonginName.gif" width="258" border="0" /> </div></td>
+                                            </tr>
+                                          </tbody>
+                                        </table></td>
+                                    </tr>
+                                  </tbody>
+                                </table></td>
+                            </tr>
+                          </tbody>
+                        </table>
  <?php
   foot();
 ?>
