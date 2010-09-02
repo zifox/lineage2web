09b68a8602e3a798b549812583f8a02593a29958
antons007 <antons007@24ad18e8-e4c2-11de-8dea-4d08a2fd72f5>
2010-09-02 16:12:22 +0000

    New Skin based on new Lineage II update Freya. Online map currently not working. Lot of images for future usage.

--- blocks/menu.php
+++ blocks/menu.php
@@ -1,9 +1,17 @@
 <?php
+//пароль
 if (!defined('IN_BLOCK')) {
     Header("Location: ../index.php");
     die();
 }
 $parse=$Lang;
+$parse['Home']=menubutton($Lang['Home']);
+$parse['Reg']=menubutton($Lang['Reg']);
+$parse['Connect']=menubutton($Lang['connect']);
+$parse['forum']=menubutton($Lang['forum']);
+$parse['statistic']=menubutton($Lang['statistic']);
+$parse['Rules']=menubutton($Lang['Rules']);
+//$parse['Donate']=menubutton($Lang['Donate']); #NOT YET FINISHED
 $parse['langpath'] = $langpath;
 $parse['lv_border'] = $_COOKIE['lang'] == 1 ? '1':'0';
 $parse['en_border'] = $_COOKIE['lang'] == 2 ? '1':'0';
